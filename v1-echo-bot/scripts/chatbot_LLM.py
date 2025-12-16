ecdcca998cbb05b770d25e7cc2a513b43c0675f9
Charlie <charlie.isra@gmail.com>
2025-12-15 16:54:55 -0800

    Add files via upload

---
+++ v1-echo-bot/scripts/chatbot_LLM.py
@@ -0,0 +1,44 @@
+
+from openai import OpenAI
+import streamlit as st
+import os
+
+st.title('Chatbot_LLM')
+
+# Set OpenAI API key from environment
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+# Set a default model
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Show previous messages from history
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Accept user input
+if prompt := st.chat_input("Waiting for your command..."):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display user message in chat container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    # Response in chat message container
+    with st.chat_message("assistant"):
+        stream = client.chat.completions.create(
+            model = st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        )
+        response = st.write_stream(stream)
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})
