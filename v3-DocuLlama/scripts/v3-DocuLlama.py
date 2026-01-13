a8725bd77fd34775b9ff71406c95b2703bc00a9f
Charlie <charlie.isra@gmail.com>
2026-01-13 11:45:22 -0800

    Add files via upload

--- v3-DocuLlama/scripts/v3-DocuLlama.py
+++ v3-DocuLlama/scripts/v3-DocuLlama.py
@@ -3,20 +3,24 @@ import streamlit as st
 from llama_index.core import SimpleDirectoryReader, VectorStoreIndex
 
 # Load path of manuals
-script_dir = os.path.dirname(__file__)
-project_dir = os.path.dirname(script_dir)
+# script_dir = os.path.dirname(__file__)
+project_dir = os.path.dirname(os.getcwd())
 data_dir = os.path.join(project_dir, 'data')
 
-# Verify path exists and print for debug
-print(f"Loading from: {data_dir}")
+
+print(f"project_dir: {project_dir}")
+print(f"data_dir: {data_dir}")
+print(f"exists: {os.path.exists(data_dir)}")
+
 if not os.path.exists(data_dir):
     raise FileNotFoundError(f"Data directory not found: {data_dir}")
 
+    
 # Streamlit decorator 
 @st.cache_resource
 # Load and cache the index
 def load_index():
-    documents = SimpleDirectoryReader(input_dir=manuals_dir).load_data()
+    documents = SimpleDirectoryReader(input_dir=data_dir).load_data()
     index = VectorStoreIndex.from_documents(documents)
     return index.as_query_engine(similarity_top_k=5)
 
